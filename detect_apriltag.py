4b85d29033f20e89a3d846075ae4a5ec6aa5a277
Irina Pirvan <alinairina1997@gmail.com>
2022-06-03 15:51:49 +0300

    recursive iteration through directories

--- detect_apriltag.py
+++ detect_apriltag.py
@@ -4,42 +4,56 @@ import argparse
 import cv2
 import os
 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--string", type=str,
+	default="aruco", help="string in name of images")
+args = vars(ap.parse_args())
+
 #global variables
 nrTagsDetected = 0
 nrFiles = 0
 
+def detect_april_markers(f):
+	global nrFiles
+	global nrTagsDetected
+
+	nrFiles += 1
+	print("\n")
+
+	# load the input image from disk and resize it
+	print("[INFO] loading image...{}".format(f))
+	image = cv2.imread(f)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.blur(gray, (2,2))
+
+	# define the AprilTags detector options and then detect the AprilTags
+	# in the input image
+	print("[INFO] detecting AprilTags...")
+	options = apriltag.DetectorOptions(families="tag36h11")
+	detector = apriltag.Detector(options)
+	results = detector.detect(gray)
+	print("[INFO] {} total AprilTags detected".format(len(results)))
+
+	# loop over the AprilTag detection results
+	for r in results:
+		tagFamily = r.tag_family.decode("utf-8")
+		if (tagFamily == 'tag36h11' and r.tag_id == 10):
+			nrTagsDetected += 1
+
+def iterate_over_files(directory):
+	for filename in os.listdir(directory):
+		f = os.path.join(directory, filename)
+
+		# checking if it is a file
+		if os.path.isfile(f) and args["string"] in filename:
+			detect_april_markers(f)
+		elif not os.path.isfile(f): # means is a directory
+			iterate_over_files(f)
+
 # assign directory
-directory = 'dataset_apriltag_aruco_cctag/spin/apriltag_images/150'
-
-#iterate over files in that directory
-for filename in os.listdir(directory):
-	f = os.path.join(directory, filename)
-
-	# checking if it is a file
-	if os.path.isfile(f):
-		nrFiles += 1
-		print("\n")
-		print(f)
-
-		# load every image and convert it to grayscale
-		print("[INFO] loading image...")
-		image = cv2.imread(f)
-		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-		gray = cv2.blur(gray, (2,2))
-
-		# define the AprilTags detector options and then detect the AprilTags
-		# in the input image
-		print("[INFO] detecting AprilTags...")
-		options = apriltag.DetectorOptions(families="tag36h11")
-		detector = apriltag.Detector(options)
-		results = detector.detect(gray)
-		print("[INFO] {} total AprilTags detected".format(len(results)))
-
-		# loop over the AprilTag detection results
-		for r in results:
-			tagFamily = r.tag_family.decode("utf-8")
-			if (tagFamily == 'tag36h11' and r.tag_id == 10):
-				nrTagsDetected += 1
+directory = '/home/irina/Documents/dataset_apriltag_aruco_cctag/spin/apriltag_images'
+iterate_over_files(directory)
 
 print("------------------------------")
 print("[Result] Tags detected: {}".format(nrTagsDetected))
--- detect_aruco_image.py
+++ detect_aruco_image.py
@@ -8,8 +8,10 @@ import os
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-t", "--type", type=str,
-	default="DICT_APRILTAG_36h11",
+	default="DICT_6X6_50",
 	help="type of ArUCo tag to detect")
+ap.add_argument("-s", "--string", type=str,
+	default="aruco", help="string in name of images")
 args = vars(ap.parse_args())
 
 # define names of each possible ArUco tag OpenCV supports
@@ -41,48 +43,56 @@ ARUCO_DICT = {
 nrTagsDetected = 0
 nrFiles = 0
 
+def detect_aruco_markers(f):
+	global nrFiles
+	global nrTagsDetected
+
+	nrFiles += 1
+	print("\n")
+
+	# load the input image from disk and resize it
+	print("[INFO] loading image...{}".format(f))
+	image = cv2.imread(f)
+	image = imutils.resize(image, width=600)
+
+	# verify that the supplied ArUCo tag exists and is supported by
+	# OpenCV
+	if ARUCO_DICT.get(args["type"], None) is None:
+		print("[INFO] ArUCo tag of '{}' is not supported".format(
+			args["type"]))
+		sys.exit(0)
+
+	# load the ArUCo dictionary, grab the ArUCo parameters, and detect
+	# the markers
+	print("[INFO] detecting '{}' tags... ".format(args["type"]))
+	arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
+	arucoParams = cv2.aruco.DetectorParameters_create()
+	(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
+		parameters=arucoParams)
+
+	# verify *at least* one ArUco marker was detected
+	if len(corners) > 0:
+		# flatten the ArUco IDs list
+		ids = ids.flatten()
+		# loop over the detected ArUCo corners
+		for (markerCorner, markerID) in zip(corners, ids):
+			print("[INFO] ArUco marker ID: {}".format(markerID))
+			if (markerID == 10):
+				nrTagsDetected += 1
+	
+def iterate_over_files(directory):
+	for filename in os.listdir(directory):
+		f = os.path.join(directory, filename)
+
+		# checking if it is a file
+		if os.path.isfile(f) and args["string"] in filename:
+			detect_aruco_markers(f)
+		elif not os.path.isfile(f): # means is a directory
+			iterate_over_files(f)
+
 # assign directory
-directory = 'dataset_apriltag_aruco_cctag/no_spin/apriltag_images'
-
-#iterate over files in that directory
-for filename in os.listdir(directory):
-	f = os.path.join(directory, filename)
-
-	# checking if it is a file
-	if os.path.isfile(f):
-		nrFiles += 1
-		print("\n")
-		print(f)
-
-		# load the input image from disk and resize it
-		print("[INFO] loading image...")
-		image = cv2.imread(f)
-		image = imutils.resize(image, width=600)
-
-		# verify that the supplied ArUCo tag exists and is supported by
-		# OpenCV
-		if ARUCO_DICT.get(args["type"], None) is None:
-			print("[INFO] ArUCo tag of '{}' is not supported".format(
-				args["type"]))
-			sys.exit(0)
-
-		# load the ArUCo dictionary, grab the ArUCo parameters, and detect
-		# the markers
-		print("[INFO] detecting '{}' tags...".format(args["type"]))
-		arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
-		arucoParams = cv2.aruco.DetectorParameters_create()
-		(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
-			parameters=arucoParams)
-
-		# verify *at least* one ArUco marker was detected
-		if len(corners) > 0:
-			# flatten the ArUco IDs list
-			ids = ids.flatten()
-			# loop over the detected ArUCo corners
-			for (markerCorner, markerID) in zip(corners, ids):
-				print("[INFO] ArUco marker ID: {}".format(markerID))
-				if (markerID == 10):
-					nrTagsDetected += 1
+directory = '/home/irina/Documents/dataset_apriltag_aruco_cctag/spin/aruco_images'
+iterate_over_files(directory)
 
 print("------------------------------")
 print("[Result] Tags detected: {}".format(nrTagsDetected))
